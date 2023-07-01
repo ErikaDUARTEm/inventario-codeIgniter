0caf145092c23d4e2f9412c2503438a189cf6183
Erika Duarte <erika.duarte.mora@gmail.com>
2023-07-01 18:18:57 -0500

    funcionalidad a edit y delete de la tabla employees

--- app/Controllers/EmployeesController.php
+++ app/Controllers/EmployeesController.php
@@ -64,4 +64,71 @@
             return redirect()->back()->with("error", $error->getMessage())->withInput();
         }
     }
+
+    public function edit($id =null){
+           
+        $employees= new Employee();
+        $query = $employees->find($id);
+        return view("/employees/edit",
+        [
+            "title" => "Editando empleados",
+            "employee" => $query
+        ]
+        );
+    }
+    public function update(){
+        $validation = $this->validate([
+            "name" => [
+                "rules" => "required|min_length[10]|max_length[30]",
+                "errors" => [
+                    "required" => "El campo nombre es obligatorio",
+                    "min_length" => "El nombre debe tener minimo 10 caracteres",
+                    "max_length" => "El nombre no puede ser mayor a 30 caracteres"
+                ]
+            ],
+            "address" => [
+                "rules" => "required|min_length[10]",
+                "errors" => [
+                    "required" => "El campo dirección es obligatorio",
+                    "min_length" => "La dirección debe tener minimo 10 caracteres"
+                ]
+            ],
+            "phone" => [
+                "rules" => "required|min_length[10]",
+                "errors" => [
+                    "required" => "El teléfono es obligatorio",
+                    "min_length" => "El teléfono debe tener minimo 10 caracteres",
+
+                ]
+            ]
+
+        ]);
+        if (!$validation) {
+            return redirect()->back()->with("errors", $this->validator->getErrors())->withInput();
+        }
+        try {
+            $id= $this->request->getPost("id");
+            $data = [
+                "name" => $this->request->getPost("name"),
+                "address" => $this->request->getPost("address"),
+                "phone" => $this->request->getPost("phone"),
+                    ];
+            $employees = new Employee();
+            $employees->update($id, $data);
+            return redirect()->to(base_url("/employees"))->with("success", "Editado exitosamente");
+        } catch (\Throwable $error) {
+            return redirect()->back()->with("error", $error->getMessage())->withInput();
+        }
+
+    }
+    public function delete($id){
+        try{
+            $employees = new Employee();
+            $employees->delete($id);
+            return redirect()->to(base_url("/employees"))->with("success", "Los datos fueron eliminados exitosamente");
+        }catch(\Throwable $error){
+            return redirect()->back()->with("error", $error->getMessage());
+        }
+       
+    }
     }
--- app/Controllers/ProvidersController.php
+++ app/Controllers/ProvidersController.php
@@ -68,4 +68,69 @@ class ProvidersController extends BaseController
             return redirect()->back()->with("error", $error->getMessage())->withInput();
         }
     }
+    public function edit($id =null){
+           
+        $providers= new Provider();
+        $query = $providers->find($id);
+        return view("providers/edit",
+        [
+            "title" => "Editando proveedores",
+            "provider" => $query
+        ]
+        );
+    }
+    public function update(){
+        $validation = $this->validate([
+            "name" => [
+                "rules" => "required|min_length[10]|max_length[30]",
+                "errors" => [
+                    "required" => "El campo nombre es obligatorio",
+                    "min_length" => "El nombre debe tener minimo 10 caracteres",
+                    "max_length" => "El nombre no puede ser mayor a 30 caracteres"
+                ]
+            ],
+            "address" => [
+                "rules" => "required|min_length[10]",
+                "errors" => [
+                    "required" => "El campo dirección es obligatorio",
+                    "min_length" => "La dirección debe tener minimo 10 caracteres"
+                ]
+            ],
+            "phone" => [
+                "rules" => "required|min_length[10]",
+                "errors" => [
+                    "required" => "El teléfono es obligatorio",
+                    "min_length" => "El teléfono debe tener minimo 10 caracteres",
+
+                ]
+            ]
+
+        ]);
+        if (!$validation) {
+            return redirect()->back()->with("errors", $this->validator->getErrors())->withInput();
+        }
+        try {
+            $id= $this->request->getPost("id");
+            $data = [
+                "name" => $this->request->getPost("name"),
+                "address" => $this->request->getPost("address"),
+                "phone" => $this->request->getPost("phone"),
+                    ];
+            $providers = new Provider();
+            $providers->update($id, $data);
+            return redirect()->to(base_url("/providers"))->with("success", "Editado exitosamente");
+        } catch (\Throwable $error) {
+            return redirect()->back()->with("error", $error->getMessage())->withInput();
+        }
+    }
+    public function delete($id){
+        try{
+            $provider = new Provider();
+            $provider->delete($id);
+            return redirect()->to(base_url("/providers"))->with("success", "El proveedor fue eliminado exitosamente");
+        }catch(\Throwable $error){
+            return redirect()->back()->with("error", $error->getMessage());
+        }
+       
+    }
 }
